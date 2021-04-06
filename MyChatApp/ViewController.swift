a8b7b5f2501641294902c4c7ba66188ef1714258
youngjun kim <youngjun.kim14@gmail.com>
2021-04-07 03:42:23 +0900

    Initial Commit

---
+++ MyChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChatApp
+//
+//  Created by youngjun kim on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
