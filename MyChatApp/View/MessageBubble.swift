16ea1e4b11d86715904515e849c16806c81175f2
youngjun kim <youngjun.kim14@gmail.com>
2021-04-07 18:36:48 +0900

    message bubble 다듬기

--- MyChatApp/View/MessageBubble.swift
+++ MyChatApp/View/MessageBubble.swift
@@ -16,7 +16,8 @@ class MessageBubble: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        bubble.layer.cornerRadius = bubble.frame.size.height / 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
